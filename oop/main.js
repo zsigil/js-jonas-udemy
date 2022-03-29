0dc3995b82d2484f3b0cd618dd1a8cc630e54b82
Zsigil <chickpox@freemail.hu>
2022-03-29 18:07:47 +0200

    prototypes

--- oop/main.js
+++ oop/main.js
@@ -7,12 +7,23 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 
   //! never create a method inside a constructor function like this
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
 };
 
 const jonas = new Person("Jonas", 1991);
 const matilda = new Person("Matilda", 2017);
 
 console.log(jonas instanceof Person); //true
+
+//PROTOTYPES
+console.log(Person.prototype === jonas.__proto__); //true
+console.log(Person.prototype.isPrototypeOf(Person)); //false!!!
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+Person.prototype.species = "homo sapiens"; //jonas.species --common property!!
+console.log(jonas.hasOwnProperty("species")); //false
