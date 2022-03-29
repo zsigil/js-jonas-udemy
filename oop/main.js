25bf3974db302c9f37fe7eec36b5ce4c9bc01949
Zsigil <chickpox@freemail.hu>
2022-03-29 15:31:03 +0200

    constructor functions

--- oop/main.js
+++ oop/main.js
@@ -1,1 +1,18 @@
 "use strict";
+
+//arrow function won't work
+const Person = function (firstName, birthYear) {
+  //instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //! never create a method inside a constructor function like this
+  this.calcAge = function () {
+    console.log(2037 - this.birthYear);
+  };
+};
+
+const jonas = new Person("Jonas", 1991);
+const matilda = new Person("Matilda", 2017);
+
+console.log(jonas instanceof Person); //true
