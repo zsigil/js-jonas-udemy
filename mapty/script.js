f386e2e34b13383113a93c5736d81b4cba888953
Zsigil <chickpox@freemail.hu>
2022-05-02 15:21:20 +0200

    basic leaflet usage

--- mapty/script.js
+++ mapty/script.js
@@ -16,6 +16,25 @@ if (navigator.geolocation) {
     function (pos) {
       const { latitude, longitude } = pos.coords;
       console.log(`https://www.google.hu/maps/@${latitude},${longitude}`);
+
+      const map = L.map("map").setView([latitude, longitude], 15);
+
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+        .openPopup();
+
+      const circle = L.circle([latitude, longitude], {
+        color: "red",
+        fillColor: "#f03",
+        fillOpacity: 0.5,
+        radius: 500,
+      }).addTo(map);
     },
     function () {
       alert("Could not get your position");
