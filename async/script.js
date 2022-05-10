db0591623487426b06b0f0d3ee7dcfe95d9e670e
Zsigil <chickpox@freemail.hu>
2022-05-10 11:10:15 +0200

    callback hell

--- async/script.js
+++ async/script.js
@@ -36,5 +36,57 @@ const getCountry = (country) => {
   });
 };
 
-getCountry("portugal");
-getCountry("usa");
+// getCountry("portugal");
+// getCountry("usa");
+
+const renderCountry = (data, className = "") => {
+  const html = `        
+        <article class="country ${className}">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.official}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(
+              +data.population / 1000000
+            ).toFixed(1)} M</p>
+            <p class="country__row"><span>🗣️</span>${
+              data.languages[Object.keys(data.languages)[0]]
+            }</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[Object.keys(data.currencies)[0]].name
+            }</p>
+          </div>
+        </article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = (country) => {
+  //ajax call country 1
+  const request = new XMLHttpRequest();
+  request.open("get", `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener("load", function () {
+    const [data] = JSON.parse(this.responseText);
+    console.log(data);
+
+    //render country 1
+    renderCountry(data);
+
+    //get neighbour country (2)
+    const neighbour = data.borders?.[0]; //works with countries with no borders
+    if (!neighbour) return;
+
+    //ajax call 2
+    const request = new XMLHttpRequest();
+    request.open("get", `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request.send();
+    request.addEventListener("load", function () {
+      const [data] = JSON.parse(this.responseText);
+      renderCountry(data, "neighbour");
+    });
+  });
+};
+
+getCountryAndNeighbour("usa");
